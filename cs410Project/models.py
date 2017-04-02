91c67b78b77fc39c018c69c57420b0f66a3e0f89
Jeff L <j.lu368@gmail.com>
2017-04-02 17:31:21 -0500

    Fixed comma, import for JSONfield

--- cs410Project/models.py
+++ cs410Project/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 class Product (models.Model):
     ProdID = models.CharField(max_length=15, primary_key=True)
@@ -7,6 +8,6 @@ class Product (models.Model):
     MeanRating = models.DecimalField(decimal_places=1, default=0)
     ModeRating = models.IntegerField(default=0)
     NumReviews = models.IntegerField(default=0)
-    GoodWords = models.JSONField()
-    BadWords = models.JSONField()
+    GoodWords = JSONField()
+    BadWords = JSONField()
     Name = models.TextField(null=True)
