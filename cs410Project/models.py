5926308ab27968345fc1522158af0866a9361241
Jeff L <j.lu368@gmail.com>
2017-04-02 17:35:34 -0500

    added decimal field req

--- cs410Project/models.py
+++ cs410Project/models.py
@@ -4,8 +4,8 @@ from django.contrib.postgres.fields import JSONField
 class Product (models.Model):
     ProdID = models.CharField(max_length=15, primary_key=True)
     #Category = models.CharField(max_length = 40)
-    Price = models.DecimalField(decimal_places=2, default=0)
-    MeanRating = models.DecimalField(decimal_places=1, default=0)
+    Price = models.DecimalField(decimal_places=2, default=0, max_digits=10)
+    MeanRating = models.DecimalField(decimal_places=1, default=0, max_digits=2)
     ModeRating = models.IntegerField(default=0)
     NumReviews = models.IntegerField(default=0)
     GoodWords = JSONField()
