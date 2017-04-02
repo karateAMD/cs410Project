682653f3121aa352521da414d783870658c47d97
Andrew Doherty <Andrew Doherty>
2017-04-02 17:22:36 -0500

    Updated table

--- cs410Project/models.py
+++ cs410Project/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class Product (models.Model):
     ProdID = models.CharField(max_length=15, primary_key=True)
     #Category = models.CharField(max_length = 40)
-    Price = models.DecimalField(decimal_places=2)
-    MeanRating = models.DecimalField(decimal_places=1)
-    ModeRating = models.IntegerField()
-    NumReviews = models.IntegerField()
-    #words
-    Name = models.CharField(max_length=50)
+    Price = models.DecimalField(decimal_places=2, default=0)
+    MeanRating = models.DecimalField(decimal_places=1, default=0)
+    ModeRating = models.IntegerField(default=0)
+    NumReviews = models.IntegerField(default=0)
+    GoodWords = models.JSONField()
+    BadWords = models.JSONField()
+    Name = models.TextField(null=True)
