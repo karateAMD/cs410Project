475637c9f2bc4b1c0a6d178420987357a2ba4deb
Andrew Doherty <Andrew Doherty>
2017-04-06 16:19:21 -0500

    Updating category in models

--- cs410Project/models.py
+++ cs410Project/models.py
@@ -3,7 +3,7 @@ from django.contrib.postgres.fields import JSONField
 
 class Product (models.Model):
     ProdID = models.CharField(max_length=15, primary_key=True)
-    #Category = models.CharField(max_length = 40)
+    Category = models.CharField(max_length = 40)
     Price = models.DecimalField(decimal_places=2, default=0, max_digits=10)
     MeanRating = models.DecimalField(decimal_places=1, default=0, max_digits=2)
     ModeRating = models.IntegerField(default=0)
