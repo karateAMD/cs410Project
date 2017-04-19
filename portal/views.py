49849e02bbe0cabccd6d7b788d9919270e33643c
Andrew Doherty <Andrew.Doherty@tksii.com>
2017-04-18 23:17:00 -0500

    Got frontend up and running

---
+++ portal/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+
+def home(request):
+    base_url = request.build_absolute_uri('/')[:-1]
+    return render_to_response('home.html', {
+        'base_url': base_url
+    })
