b2b709134b4d7a9c67a1fc42849c58adc6043ec0
Andrew Doherty <Andrew.Doherty@tksii.com>
2017-04-19 15:39:43 -0500

    Ignoring .pyc files

--- cs410Project/settings.py
+++ cs410Project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'cs410Project',
+    'portal',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
