758cbc3cf67b6bd9c75e187c12fd15051851bf15
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-05-07 14:54:19 -0500

    Add treemap1 page

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^search1$', views.treemap1),
 ]
--- portal/views.py
+++ portal/views.py
@@ -7,3 +7,6 @@ def home(request):
     return render_to_response('home.html', {
         'base_url': base_url
     })
+
+def treemap1(request):
+    return render_to_response('treemap1.html')
