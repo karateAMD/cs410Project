c7bb222d2e08acccc7c7939418f4a24df08d21e8
Andrew Doherty <Andrew.Doherty@tksii.com>
2017-05-07 18:02:48 -0500

    Fixed urls for search results

--- portal/urls.py
+++ portal/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^search1$', views.treemap1),
-    url(r'^search2$', views.treemap2),
-    url(r'^search3$', views.treemap3),
-    url(r'^search4$', views.treemap4),
+    url(r'^search=RomaTheEternalCity$', views.treemap1),
+    url(r'^search=LetThemEat$', views.treemap2),
+    url(r'^search=TheThirst$', views.treemap3),
+    url(r'^search=ManWithXrayEyes$', views.treemap4),
 ]
